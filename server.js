e7b99cbd181c8f06048268d37479ca533df12bf6
Landon McKell <lmckell@nerdunited.com>
2024-05-30 15:37:26 -0600

    chore: Add CORS middleware for handling cross-origin requests

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./src/config/database.js";
 import userRoutes from "./src/routes/user.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 connectDB();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/users", userRoutes);
 
